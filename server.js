7c50f478f3e082ad132bf6ecc2ba9f2dd5e7843a
Rabie AL-Ashmali <rabiealashmali@gmail.com>
2022-12-25 12:06:03 +0000

    creating a new  message with simple validation and adding the time the message was created/sent

--- server.js
+++ server.js
@@ -16,68 +16,27 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-let messages = [welcomeMessage]
+let messages = [welcomeMessage];
 
-
-// level3 [5] read only text whose text contains a given substring
-// teniolao-cyf-chat-server.glitch.me/messages/search?text=express
-app.get("/messages/search", (req, res)=> {
-  const { term } = req.query
-  console.log(term)
-  
-  const filterMessages = messages.filter(message => message.text.toLowerCase().includes(term.toLowerCase()))
-  console.log(filterMessages)
-  res.send(filterMessages);
-});
-
-//[2] read all messages
-app.get("/messages", (req, res) => {
-  res.json(messages);
-});
-
-//read only the most recent 10 messages
-app.get("/messages/latest", (req, res) => {
-  //for(let i=)
- const filterMessages = messages.filter((message, index) => messages.length-10 <= index)
- res.send(filterMessages)
-});
-
-//[3] read one message specified by id
-app.get("/messages/:id", function (req, res) {
-  const id = req.params.id;
-  messages = messages.filter(
-    (message) => message.id === Number(id)
-  );
-    res.status(200).send(messages);
-});
-
-// [1] create a new message
+// creating a message with simple validation and adding the time the message was created/sent
 app.post("/messages", (req, res) => {
-  const { from, text } = req.body;
-  // console.log(req.body)
-  const ourMessageObject = {
+  const { from, text } = req.body; // getting the data from both the name and message fields by targetting the name attribute of both
+  const messageObj = {
     id: messages.length, 
-    from,
-    text,
+    from, 
+    text, 
     timeSent: new Date().toLocaleDateString()
   }
-  if(from.length === 0 || text.length === 0){
-    return res.status(400).send("please complete body")
-  } else {
-     messages.push(ourMessageObject);
+  if (!from || !text) {
+    return res.status(404).send({ message: "please fill in both the name and message"})
+  }else{
+  messages.push(messageObj);
   }
-});
-
-//[4] delete a message by id
-app.delete("/messages/:id", (req, res) => {
-  const id = req.params.id;
-  messages = messages.filter((message) => message.id !== Number(id));
-  res.json(messages);
 });
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 app.listen(process.env.PORT || 3000, () => {
-  console.log("server running!");
+  console.log("server running on port 3000!");
 });
